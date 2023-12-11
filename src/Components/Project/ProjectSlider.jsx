544d7faa4457c4eb908d40376e2d75207d458b88
AsibHasanRiyad <asibhasanriyad@gmail.com>
2023-12-11 15:52:56 +0600

    basic work of project section is done

--- src/Components/Project/ProjectSlider.jsx
+++ src/Components/Project/ProjectSlider.jsx
@@ -1,11 +1,33 @@
 /* eslint-disable react/prop-types */
 
+import { FaGithub, FaLink } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-const ProjectSlider = ({data}) => {
+const ProjectSlider = ({ data }) => {
   return (
-    <div className=" overflow-hidden shadow-xl">
-    <img src={data.image} alt="" />
-  </div>
+    <div >
+      <div className=" w-full h-[280px]">
+        <img
+          className="   w-[247px] h-[270px] bottom-0 shadow-2xl"
+          src={data.image}
+          alt=""
+        />
+      </div>
+      <div className=" px-2 py-2 h-full">
+        <h4 className=" text-2xl text-gray-200 font-semibold mb-2">
+          {data.name}
+        </h4>
+        <p className=" text-gray-300">{data.details}</p>
+        <div className=" flex items-center gap-5 text-4xl mt-4 text-gray-300">
+          <Link className=" text-4xl" to={data.gitHub}>
+          <FaGithub></FaGithub>
+          </Link>
+          <Link to={data.live}>
+          <FaLink className=" text-4xl"></FaLink>
+          </Link>
+        </div>
+      </div>
+     </div>
   );
 };
 
--- src/Components/Project/Projects.jsx
+++ src/Components/Project/Projects.jsx
@@ -37,6 +37,16 @@ const projectData = [
     Link: "/",
     gitHub: "/",
   },
+  {
+    image:
+      "https://i.ibb.co/P4xCkYt/Pink-Blue-Powerful-Sunrise-Over-the-Sea-Quote-Instagram-Post-3.png",
+    name: "Connectopia",
+    category: "React js",
+    details:
+      "On a continual quest for learning, I leverage a strong work ethic to apply my React development skills",
+    Link: "/",
+    gitHub: "/",
+  },
 ];
 
 const Projects = () => {
@@ -44,30 +54,35 @@ const Projects = () => {
     <section className="relative  pb-48">
       <div className=" container mx-auto ">
         <div className=" max-w-[400px] mx-auto lg:mx-0 text-center lg:text-left mb-12 lg:h-[400px] flex flex-col justify-center  items-center lg:items-start ">
-          <h1 className=" text-transparent bg-gradient-to-br bg-clip-text from-indigo-500 via-purple-500 to-pink-500 text-5xl font-semibold">ALl Project</h1>
+          <h1 className=" text-transparent bg-gradient-to-br bg-clip-text from-indigo-500 via-purple-500 to-pink-500 text-5xl font-semibold">
+            ALl Project
+          </h1>
           <p className=" text-xl text-gray-200 py-4">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
             voluptate?
           </p>
         </div>
         {/* slide */}
-       <div className="lg:max-w-[800px] lg:absolute right-0 top-0">
-       <Swiper className=" h-[480px]"
-       slidesPerView={1}
-       breakpoints={{640:{
-        slidesPerView : 2
-       }}}
-       spaceBetween={30}
-       modules={[Pagination]}
-       pagination={{clickable:true}}
-       >
-            {
-                projectData.map((data, index) => <SwiperSlide key={index}> 
+        <div className="lg:max-w-[800px] lg:absolute right-0 top-0">
+          <Swiper
+            className=" h-[480px]"
+            slidesPerView={1}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+              },
+            }}
+            spaceBetween={30}
+            modules={[Pagination]}
+            pagination={{ clickable: true }}
+          >
+            {projectData.map((data, index) => (
+              <SwiperSlide key={index}>
                 <ProjectSlider data={data} />
-                </SwiperSlide> )
-            }
-        </Swiper>
-       </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </section>
   );
