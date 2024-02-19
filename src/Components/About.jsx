ebd77815423aa070a3706d766b22b53c8f15fef9
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-02-19 12:55:19 +0600

    no

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,15 +3,15 @@ import Skills from "./Skills";
 const About = () => {
   return (
     <div>
-      <div name="about" className=" pt-0 lg:pt-10 pb-5">
-        <h1 className="text-transparent bg-gradient-to-br bg-clip-text from-indigo-500 via-purple-500 to-pink-500 text-5xl text-center py-4 lg:py-10 font-bold">
+      <div name="about" className="pt-0 pb-5  lg:pt-10">
+        <h1 className="py-4 text-5xl font-bold text-center text-transparent bg-gradient-to-br bg-clip-text from-indigo-500 via-purple-500 to-pink-500 lg:py-10">
           About Me
         </h1>
       </div>
-      <div className=" grid grid-cols-1 lg:grid-cols-2 items-center justify-between pb-6 md:pb-10 lg:pb-20">
-        <div className=" flex justify-center">
+      <div className="grid items-center justify-between grid-cols-1 pb-6  lg:grid-cols-2 md:pb-10 lg:pb-20">
+        <div className="flex justify-center ">
           <img
-            className=" w-72 lg:w-96 mb-10 lg:mb-0"
+            className="mb-10  w-72 lg:w-96 lg:mb-0"
             data-aos="fade-right"
             data-aos-duration="600"
             data-aos-easing="ease-in-sine"
